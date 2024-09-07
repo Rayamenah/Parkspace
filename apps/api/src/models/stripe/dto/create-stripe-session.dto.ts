831db9be07bc236dd1d2891130e6d819ed3adfd2
Saibot <101467129+Rayamenah@users.noreply.github.com>
2024-09-07 03:07:43 +0100

    Delete apps/api/src/models/stripe directory

--- apps/api/src/models/stripe/dto/create-stripe-session.dto.ts
+++
@@ -1,8 +0,0 @@
-import { TotalPrice } from '@parkspace/util/types'
-import { CreateBookingInput } from 'src/models/bookings/graphql/dtos/create-booking.input'
-
-export class CreateStripeDto {
-  uid: string
-  totalPriceObj: TotalPrice
-  bookingData: CreateBookingInput
-}
